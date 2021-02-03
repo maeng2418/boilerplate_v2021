c768ef9936d87774ac16ec5f15fc71cf7dc1897a
maeng2418 <maeng2418@naver.com>
2021-02-03 21:14:30 +0900

    [fix] className 제거

--- src/components/templates/Sample/Sample.tsx
+++ src/components/templates/Sample/Sample.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import styles from './Sample.module.scss';
-import classNames from 'classnames/bind';
 import StyledSample_01 from './SampleStyles';
-
-const cx = classNames.bind(styles);
 
 const Sample: React.FC = (props) => {
   return (
-    <div className={cx('sample')}>
+    <div>
       <StyledSample_01 {...props}>샘플 템플릿</StyledSample_01>
     </div>
   );
